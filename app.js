cbc071a40b016fc7d20b5aeba0e591d6d5b01155
Sanket Kharat <140420720+sanketkharat28@users.noreply.github.com>
2023-12-03 23:47:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="hello-container">
+      <p className="hello-text">Hello</p>
+    </div>
+  );
+}
+
+export default App;
